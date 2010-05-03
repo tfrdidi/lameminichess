756a6cbd35446c86a82ed387f72714cecc5274f3
clemens.henker <clemens.henker@efc36dc2-30b7-d39d-9e85-077070bed4e3>
2010-05-03 20:27:51 +0000

    - Copyright prolog added - Board: JavaDoc added, beautified

--- src/lmc/Board.java
+++ src/lmc/Board.java
@@ -1,18 +1,29 @@
-/**
- * 
+/*
+ * Copyright © 2010 Stefan Liebler, Clemens Henker, Lukas Hahmann. All rights reserved.
  */
 package lmc;
 
 import java.io.*;
-import java.lang.reflect.Array;
-
 
 /**
- * @author sli
+ * The class <code>Board</code> represents a 5x6 mini chess board 
+ * and basic I/O to manage fundamental communication.
+ *
+ * @author  
+ * 	Stefan Liebler <liebler.stefan@googlemail.com>,
+ *  Clemens Henker <clemens.henker@googlemail.com>, 
+ *  Lukas Hahmann <tfr.didi@googlemail.com>
+ * @version 0.1 2010/05/03
+ * @since 0.1
  */
 public class Board {
 
-	public Board() throws Exception{
+    /**
+     * Constructs a new mini chess board with default start configuration.
+     * 
+     * @exception  Exception  If default initialization string is malformed
+     */
+	public Board() throws Exception {
 		initBoard("1 W\n"
 		        +"kqbnr\n"
 		        +"ppppp\n"
@@ -22,11 +33,25 @@ public class Board {
 		        +"RNBQK\n");
 	}
 	
-	public Board(String str) throws Exception{
-		initBoard(str);
+	/**
+     * Constructs a new mini chess board at an individual state per <code>String</code>.
+     * 
+     * @param state A string, representing the game start state
+     * 
+     * @exception  Exception  If <code>state</code> is malformed
+     */
+	public Board(String state) throws Exception {
+		initBoard(state);
 	}
 	
-	public Board(InputStream reader) throws Exception{
+	/**
+     * Constructs a new mini chess board at an individual state per <code>InputStreamReader</code>.
+     * 
+     * @param reader An input stream, which initializes the board
+     * 
+     * @exception  Exception  If reader is malformed
+     */
+	public Board(InputStream reader) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(reader));
 		
 		String all = "";
@@ -44,22 +69,27 @@ public class Board {
 	 */
 	private char[][] board;
 	
-	private static  String validBoardCharacters = ".PRNBQKprnbqk";
+	private static String validBoardCharacters = ".PRNBQKprnbqk";
 	
 	private int runCount;
 	private char currentPlayer;
 	
-	
-	private void initBoard(String str) throws Exception{
+	/**
+     * Initializes the <code>Board</code>.
+     * 
+     * @param initState A state, representing a board state
+     * 
+     * @exception  Exception  If initState is malformed
+     */
+	private void initBoard(String initState) throws Exception{
 		board = new char[6][5];
 		
-		String[] lines = str.split("\n");
+		String[] lines = initState.split("\n");
 		
-		if (lines.length < 7){
-			throw new Exception("bad Board Format - number of lines");
+		if (lines.length != 7){
+			throw new Exception("Bad initState format - Parameter needs 7 lines (has " + lines.length + " lines)");
 		}
 		
-				
 		//read game state
 		String[] lineOne = lines[0].split(" ");
 		runCount = Integer.parseInt(lineOne[0]);
@@ -69,15 +99,15 @@ public class Board {
 		//read board state
 		char currchar =' ';
 		for (int iRow = 1; iRow < 7; iRow++) {
-			if (lines[iRow].length() < 5){
-				throw new Exception("bad Board Format - number of columns is false in row " + iRow);
+			if (lines[iRow].length() != 5){
+				throw new Exception("Bad initState format - Column #" + iRow + " needs 5 values (has " + lines[iRow].length() + " values)");
 			}
 					
 			for (int iColumn = 0; iColumn < 5; iColumn++) {
 				currchar = lines[iRow].charAt(iColumn);
 				if (!isBoardCharValid(currchar))
 				{
-					throw new Exception("bad Board Format - '" + currchar + "' is not a valid character");
+					throw new Exception("Bad initState format - '" + currchar + "' is not a valid character");
 				}
 				board[6 - iRow][iColumn] = currchar;
 			}
@@ -85,15 +115,22 @@ public class Board {
 		
 	}
 	
+	/**
+     * Returns <code>true</code> if character is element of <code>validBoardCharacters</code>.
+     * 
+     * @param currchar A character to test
+     */
 	private boolean isBoardCharValid(char currchar) {		
 		return validBoardCharacters.contains(new String(new char[]{currchar}));		
 	}
 
+	/**
+     * Returns a <code>String</code> representing the actual board state.
+     */
 	@Override
 	public String toString() {
 
 		String ret = runCount + " " + currentPlayer + "\n";
-
 		
 		for (int iRow = board.length - 1; iRow >= 0; iRow--) {
 			for (int iCol = 0; iCol < board[iRow].length; iCol++) {
@@ -105,14 +142,25 @@ public class Board {
 		return ret;
 	}
 	
-	public void print(OutputStream os) throws IOException{
+	/**
+     * Returns an <code>OutputStream</code> representing the actual board state.
+     * 
+     * @exception	IOException	If problem with <code>OutputStream</code>
+     */
+	public void print(OutputStream os) throws IOException {
 		OutputStreamWriter ow = new OutputStreamWriter(os);
 		ow.write(this.toString());
 		ow.flush();
 	}
 	
-	public static Board fromFile(String fileName) throws Exception{
-	
+	/**
+     * Returns a <code>Board</code> representing the actual board state from a file.
+     * 
+     * @param		fileName	The name of the file containing the board state
+     * 
+     * @exception	Exception	If some problem with <code>FileInputStream</code>
+     */
+	public static Board fromFile(String fileName) throws Exception {
 		FileInputStream fis = new FileInputStream(fileName);
 		Board b = null;
 		b = new Board(fis);
@@ -120,29 +168,53 @@ public class Board {
 		return b;
 	}
 	
-	public static void toFile(Board b, String fileName) throws Exception{
-		
+	/**
+     * Saves the current state of the <code>Board</code> in a file.
+     * 
+     * @param		b			The Board which state should be saved
+     * @param		fileName	The name of the file where to write the state information
+     * 
+     * @exception	Exception	If some problem with <code>FileOutputStream</code>
+     */
+	public static void toFile(Board b, String fileName) throws Exception {
 		FileOutputStream fos = new FileOutputStream(fileName, true);
 		b.print(fos);
 		fos.flush();
 		fos.close();
 	}
 
-	private char getSquare(Square location){
+	/**
+     * Returns the value of a <code>Square</code>.
+     * 
+     * @param		location	Value of this <code>Square</code> will be returned
+     */
+	private char getSquare(Square location) {
 		return board[location.getRow()][location.getColumn()];
 	}
 	
-	private void setSquare(Square location, char piece){
+	/**
+     * Sets the value of a <code>Square</code>.
+     * 
+     * @param		location	Value of this <code>Square</code> will be set
+     * @param		piece		New value of the <code>Square</code>
+     */
+	private void setSquare(Square location, char piece) {
 		 board[location.getRow()][location.getColumn()] = piece;
 	}
 	
-	public void move(Move move){
+	/**
+     * Processes a move on the board.
+     * 
+     * @param		move		The <code>Move</code> to do
+     */
+	public void move(Move move) {
 		char currPiece = getSquare(move.from);
 		setSquare(move.from, '.');
 		setSquare(move.to, currPiece);
 	}
 	
-	public static void main(String args[]){
+	
+	public static void main(String args[]) {
 		
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
@@ -169,27 +241,5 @@ public class Board {
 		}catch(Exception e){
 			System.out.println(e);
 		} 
-		
-//		try {
-//			
-//			Board b = new Board("1 W\n"
-//					+"kqnbr\n"
-//					+"ppppp\n"
-//					+".....\n"
-//					+".....\n"
-//					+"PPPPP\n"
-//					+"RNBQK");
-//			System.out.print(b);
-//
-//			Board.toFile(b, args[0]);
-//		} catch (Exception e) {
-//			System.out.println("Error while writing!!!");
-//			e.printStackTrace();
-//			
-//		}
-//		File f = new File("out.txt");
-//		if (f.exists()){
-//			System.out.println(f.getAbsolutePath());
-//		}
 	}
 }
